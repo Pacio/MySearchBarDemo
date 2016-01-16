3c10800d0cf347365ca2f62537dcd4efd197161d
LockingBoy <4995125@163.com>
2016-01-16 20:55:09 +0800

    Initial Commit

---
+++ UIsearchBar_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIsearchBar_Demo
+//
+//  Created by summer on 16/1/16.
+//  Copyright © 2016年 pAcio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIsearchBar_Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIsearchBar_Demo
+//
+//  Created by summer on 16/1/16.
+//  Copyright © 2016年 pAcio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
